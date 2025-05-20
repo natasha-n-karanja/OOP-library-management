7a6bd939eefff2864ab1c7b500119ff47846201a
natashankaranja <natashankaranja@gmail.com>
2025-05-20 16:19:34 +0300

    added library context

---
+++ library.py
@@ -0,0 +1,10 @@
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def show_books(self):
+        for book in self.books:
+            book.display_info()
